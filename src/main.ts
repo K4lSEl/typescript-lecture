3e7a68c47e033ab6040abe0c56dd3083de75f08e
Sata Kaisei <nmap7vk3mweqtbtzst1z@gmail.com>
2026-07-02 11:45:45 +0900

    mainを変更

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { Enemy } from "./enemy";
+import { Warrior } from "./warrior";
 
 const slime = new Enemy("スライム", 50, 10);
+const warrior = new Warrior("勇者", 100, "エクスカリバー", 20);
+
 slime.showStatus();
-slime.attack();
+slime.attack(warrior);
 slime.takeDamage(40);
-slime.showStatus();
-slime.attack();
+
+warrior.showStatus();
+warrior.attack(slime);
+warrior.takeDamage(10);
