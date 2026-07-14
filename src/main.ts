9b4ef2bfcc83c6cf7ccc77770be5e0d35f42105e
Sata Kaisei <nmap7vk3mweqtbtzst1z@gmail.com>
2026-07-14 11:08:32 +0900

    mainに蘇生を実装

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,30 @@ import { Archer } from "./archer";
 import { Mage } from "./mage";
 import { HolyPriest } from "./holy-priest";
 
-const slime = new Enemy("スライム", 50, 10);
+const slime = new Enemy("スライム", 150, 30);
 const warrior = new Warrior("勇者", 100, "エクスカリバー", 20);
 const archer = new Archer("弓氏", 120, 15, 20);
 const mage = new Mage("魔法使い", 200, 20);
-const holypriest = new HolyPriest("僧侶", 300, 0);
+const priest = new HolyPriest("僧侶", 300, 0);
 
 while (!warrior.isDead() && !slime.isDead()) {
   warrior.attack(slime);
   slime.showStatus();
 
   if (slime.isDead()) {
-    console.log(`${warrior.getName}を倒した`);
+    console.log(`${warrior.getName()}を倒した`);
     break;
   }
 
   slime.attack(warrior);
-  warrior.showStatus;
+  warrior.showStatus();
 
   if (warrior.isDead()) {
-    console.log(`${slime.getName}は倒された`);
+    console.log(`${warrior.getName()}は倒された`);
+
+    priest.revive(warrior);
+    console.log(`${priest.getName()}は${warrior.getName()}を復活させた`);
+    warrior.showStatus();
     break;
   }
 }
