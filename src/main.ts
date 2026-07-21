9016e169a41b8806658549c5ade346491f000bb3
Sata Kaisei <nmap7vk3mweqtbtzst1z@gmail.com>
2026-07-21 20:37:32 +0900

    カスタムエラーの追加

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
+class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
 // src/main.ts
 function divide(a: number, b: number): number {
   // TODO 1: b が 0 なら throw new Error(...)、そうでなければ a / b を返す
   if (b === 0) {
-    throw new Error("0で割ることはできません");
+    throw new ValidationError("0で割ることはできません");
   }
   return a / b;
 }
 
 try {
   // TODO 2: divide を呼んで、結果を console.log で表示
-  const result = divide(10, 2);
-  console.log(result);
+  console.log(divide(10, 2));
+  console.log(divide(10, 0));
 } catch (error) {
-  // TODO 3: (error as Error).message を console.error で表示
-  console.error((error as Error).message);
+  if (error instanceof ValidationError) {
+    console.error(error);
+  }
 } finally {
   // TODO 4: 必ず実行されるログ
   console.log("計算を終了しました");
